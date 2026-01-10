6c46db1c1742e4c5faaf00ec554f449ded9175c4
krineshsuthar-rgb <krineshsuthar@gmail.com>
2026-01-10 08:10:59 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+
+
+// "33" => 33
+//"33abc" => nan
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(stringNumber);
+ 
