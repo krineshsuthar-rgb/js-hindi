47df52088e2c9ab63ca0456ad89d339b37634b47
krineshsuthar-rgb <krineshsuthar@gmail.com>
2026-01-10 06:05:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code ass newer version
+
+// alert(3 + 3)// we are using node.js, not browser
+
+console.log(3 + 3)// code readability should be high
+
+console.log("krinesh")
+
+
+let name = "krinesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power  53
+//bigint
+//string =>""
+//BOOLEAN => true/false
+// null +> standalone value
+// undefined =>
+//symbol => unique
+
+//object 
+
+console.log(typeof undefined);
+console.log(typeof null);
+
