4794ee6850be1041cee13230b9b0aad136363c3f
krineshsuthar-rgb <krineshsuthar@gmail.com>
2026-01-11 05:00:07 +0000

    change in location

---
+++ datatypes-summary.js
@@ -0,0 +1,37 @@
+ // Primitive
+
+ //  7 types : string, Number, Boolean, null,undefined, symbol,Bigint.
+
+const score = 100
+
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+let userEmail;
+
+//const id = symbol('123')
+//const anotherId = symbol('123')
+
+
+      
+//console.log(id === anotherId);
+
+ // Reference (non primitive)
+
+ // Array, objects, Functions
+
+ const heros = ["shaktiman", 'naagraj',"doga"]
+ let myObj = {
+    name:"krinesh",
+    age: 21,
+
+ }
+
+ const myFunction = function(){
+    console.log("hello world");
+ }
+
+ console.log(typeof scoreValue)
