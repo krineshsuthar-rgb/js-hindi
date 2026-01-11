7547a70fc5885328833e270d7b5b47ab34490b06
krineshsuthar-rgb <krineshsuthar@gmail.com>
2026-01-11 10:23:46 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++Maths++++++++++++++++++++++++++++++=
+
+console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4, 3, 6 ,8));
+// console.log(Math.max(4, 3, 6 ,8));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const Min = 10
+const Mxa = 20
+
+log
